1f7f70f7731314bc3f5f9372418ab1be4a65daa1
huochengyan <929914120@qq.com>
2022-09-13 11:02:03 +0800

    create MVVM

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcyWpfApp.ViewModels
+{
+    public class LoginViewModel: ObservableObject
+    {
+    }
+}
